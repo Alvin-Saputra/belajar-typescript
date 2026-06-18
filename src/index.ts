6403dc66dce8ca11d58355dca49171e66e34d367
alvinsaputra123 <alvin.saputra20162016@gmail.com>
2026-06-18 09:01:45 +0700

    test

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ const sapaan: string = "Halo, ini kode TypeScript pertamaku!";
 const year: number = 2026;
 
 console.log(`${sapaan} Sekarang tahun ${year}.`);
+
+console.log("Halo, ini kode TypeScript pertamaku! Sekarang tahun 2026.");
