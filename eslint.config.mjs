ac24ce3d7a6545dd48e80df5fe3df6b90bb4d5db
alvinsaputra123 <alvin.saputra20162016@gmail.com>
2026-06-28 21:58:14 +0700

    second commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,23 @@ import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { globals: globals.browser } 
+  },
   tseslint.configs.recommended,
+  // Tambahkan objek ini di paling bawah untuk menimpa aturan bawaan
+  {
+    rules: {
+      // Mematikan aturan unused-vars dari TypeScript
+      "@typescript-eslint/no-unused-vars": "off",
+      
+      // Opsional: Mematikan aturan unused-vars dari JavaScript bawaan 
+      // (berjaga-jaga jika file .js kamu juga kena error serupa)
+      "no-unused-vars": "off",
+      "prefer-const": "off"
+    }
+  }
 ]);
